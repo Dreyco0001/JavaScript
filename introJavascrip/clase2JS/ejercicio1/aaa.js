32f4f2317ae2ef55af5c874e7dbf2fc845032a23
Dreyco0001 <dreycoarak00@gmail.com>
2022-05-31 22:32:35 -0400

    avance clase

---
+++ introJavascrip/clase2JS/ejercicio1/aaa.js
@@ -0,0 +1,13 @@
+var bello = prompt("Eres bellisim@")
+
+
+console.log("Eres bellisim@" + bello);
+var texto = "Eres bellisimo@" +bello;
+
+console.log(bello)
+
+
+if (bello== "si") {console.log("ciertamente")}
+
+if(bello== "no") 
+{console.log("No te creo")}
---
+++ introJavascrip/clase2JS/ejercicio2/tito.js
@@ -0,0 +1,9 @@
+
+
+var numerosPares = prompt("numeros divisibles del 1 al 10")
+
+console.log ("numeros divisibles del 1 al 10" + numerosPares)
+console.log(numerosPares)
+
+if(numerosPares/2==1) {console.log(numerosPares+ "si es divisible")}
+else if {console.log("no es posible")} 
