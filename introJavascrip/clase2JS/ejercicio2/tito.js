c054a74bb44d9e6e5f14efc497a3df775fb98459
Dreyco0001 <dreycoarak00@gmail.com>
2022-06-01 15:51:36 -0400

    solucion de ejercicios; copiar a mano y probar

--- introJavascrip/clase2JS/ejercicio2/tito.js
+++ introJavascrip/clase2JS/ejercicio2/tito.js
@@ -1,9 +1,13 @@
 
 
-var numerosPares = prompt("numeros divisibles del 1 al 10")
+var numero= prompt("ingresa un numero")
 
-console.log ("numeros divisibles del 1 al 10" + numerosPares)
-console.log(numerosPares)
 
-if(numerosPares/2==1) {console.log(numerosPares+ "si es divisible")}
-else if {console.log("no es posible")} 
+
+// console.log ("numeros divisibles del 1 al 10" + numerosPares)
+// console.log(numerosPares)
+
+// if(numerosPares/2==0) {console.log(numerosPares+ "si es divisible")}
+// else if {console.log("no es posible")} 
+
+if (num)
