585343572e2a28d40bb1fd14b96befa87b878fa8
Dreyco0001 <dreycoarak00@gmail.com>
2022-06-02 22:35:41 -0400

    textos agregados

--- introJavascrip/clase3/index.js
+++ introJavascrip/clase3/index.js
@@ -1,19 +1,26 @@
 var participantes = ['Ana', 'Oswaldo', 'Raul', 'Celia', 'Maria', 'Antonio'];
 
-// var participantesEdit = participantes.splice (',')
+//cambiamos de puesto a Cecilia y Raul
 console.log(participantes)
 participantes[2]= 'cecilia'
 participantes[3]= 'Raul'
 console.log(participantes)
-
+ 
+//elimina al participante antonio
 console.log(participantes)
 participantes.pop(5)
 console.log(participantes)
 
+//Se nos unen a la fiesta Roberto y Amaya en el top 
 console.log(participantes)
 console.log(participantes.splice(1,0,'Roberto','Amaya'))
 console.log(participantes)
 
+
+//Toma el primer puesto Marta
 console.log(participantes)
 participantes.unshift('Marta')
 console.log(participantes)
+
+
+//de izquierda a derecha se lee los puestos de los participantes
